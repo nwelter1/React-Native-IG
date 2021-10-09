1432697afc01171eeed3cb17bd19ce858edd36c5
nwelter1 <nwelter1@gmail.com>
2021-10-08 20:52:13 -0500

    setup user registration with firebase

--- App.js
+++ App.js
@@ -3,12 +3,31 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_I
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
+}
+
+
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={ LandingScreen } options={{ headerShown: false }}/>
+        <Stack.Screen name="Register" component={ RegisterScreen } />
       </Stack.Navigator>
     </NavigationContainer>
   );
